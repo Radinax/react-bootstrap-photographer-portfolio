22a5a91c5ba4ac3645f0f121d8cfd00323936097
Adrian <adrberia@gmail.com>
2020-06-05 21:54:00 -0400

    Updates

---
+++ precache-manifest.e99fe9467ec1dfa34419e06ebf605383.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43fcf718ae2498b148e2ef7af0f2db0f",
+    "url": "/react-bootstrap-photographer-portfolio/index.html"
+  },
+  {
+    "revision": "2e186b34f55fabeaee3a",
+    "url": "/react-bootstrap-photographer-portfolio/static/css/2.cbdddf7c.chunk.css"
+  },
+  {
+    "revision": "4f9efd0bb0898ee816bc",
+    "url": "/react-bootstrap-photographer-portfolio/static/css/main.a8748a96.chunk.css"
+  },
+  {
+    "revision": "2e186b34f55fabeaee3a",
+    "url": "/react-bootstrap-photographer-portfolio/static/js/2.bf7cd567.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-bootstrap-photographer-portfolio/static/js/2.bf7cd567.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f9efd0bb0898ee816bc",
+    "url": "/react-bootstrap-photographer-portfolio/static/js/main.44f56042.chunk.js"
+  },
+  {
+    "revision": "0d89ed75904a3d1f9a5f",
+    "url": "/react-bootstrap-photographer-portfolio/static/js/runtime-main.f7130db0.js"
+  },
+  {
+    "revision": "8c3986fc66a171fd1d0c9848f04a5a46",
+    "url": "/react-bootstrap-photographer-portfolio/static/media/business.8c3986fc.jpeg"
+  },
+  {
+    "revision": "3fed77f6130c626aa6373a2d2a15d0fd",
+    "url": "/react-bootstrap-photographer-portfolio/static/media/camera.3fed77f6.png"
+  },
+  {
+    "revision": "8506cac478b99386d6473fb96ce7b825",
+    "url": "/react-bootstrap-photographer-portfolio/static/media/customer1.8506cac4.jpeg"
+  },
+  {
+    "revision": "07505aa347287419e7019cee347f8521",
+    "url": "/react-bootstrap-photographer-portfolio/static/media/customer2.07505aa3.jpeg"
+  },
+  {
+    "revision": "7567ad82907918d7f9a7ac2badfc937a",
+    "url": "/react-bootstrap-photographer-portfolio/static/media/customer3.7567ad82.jpeg"
+  },
+  {
+    "revision": "da58bf959a1228f5b902dc67b9dcdfbc",
+    "url": "/react-bootstrap-photographer-portfolio/static/media/family.da58bf95.jpeg"
+  },
+  {
+    "revision": "6d1bb973ce7df6525e5ed19767ccd539",
+    "url": "/react-bootstrap-photographer-portfolio/static/media/fashion.6d1bb973.jpeg"
+  },
+  {
+    "revision": "ab88515a1c2d9b3956c98ac4f1352955",
+    "url": "/react-bootstrap-photographer-portfolio/static/media/form-img.ab88515a.jpeg"
+  },
+  {
+    "revision": "ec6e17053d09657a45b6aa796d4e384e",
+    "url": "/react-bootstrap-photographer-portfolio/static/media/header-img.ec6e1705.jpeg"
+  },
+  {
+    "revision": "26c7bbb8764aab20a8d138f1f4ddb198",
+    "url": "/react-bootstrap-photographer-portfolio/static/media/img1.26c7bbb8.jpeg"
+  },
+  {
+    "revision": "274344ad3a023404b27f1e4cdace228a",
+    "url": "/react-bootstrap-photographer-portfolio/static/media/img10.274344ad.jpeg"
+  },
+  {
+    "revision": "75d20fccd55c4bdd88883867d0c9dd81",
+    "url": "/react-bootstrap-photographer-portfolio/static/media/img11.75d20fcc.jpeg"
+  },
+  {
+    "revision": "5523764321e9b88b23de5bea86c0c2f0",
+    "url": "/react-bootstrap-photographer-portfolio/static/media/img12.55237643.jpeg"
+  },
+  {
+    "revision": "7b97a2743954840a25e6b9bb8b0ae67d",
+    "url": "/react-bootstrap-photographer-portfolio/static/media/img13.7b97a274.jpeg"
+  },
+  {
+    "revision": "4893d9209cec6fcfc52abb0ba887720d",
+    "url": "/react-bootstrap-photographer-portfolio/static/media/img14.4893d920.jpeg"
+  },
+  {
+    "revision": "7edbb244ff9fdd8a05f7a9506d6b3098",
+    "url": "/react-bootstrap-photographer-portfolio/static/media/img15.7edbb244.jpeg"
+  },
+  {
+    "revision": "219c66ed2e2000c04b9ad39dee5e1e43",
+    "url": "/react-bootstrap-photographer-portfolio/static/media/img16.219c66ed.jpeg"
+  },
+  {
+    "revision": "5b46ba0600f75bd7cf74ea3289c37d69",
+    "url": "/react-bootstrap-photographer-portfolio/static/media/img2.5b46ba06.jpeg"
+  },
+  {
+    "revision": "ec73cd30c2fbed26e5cf5318544241fa",
+    "url": "/react-bootstrap-photographer-portfolio/static/media/img3.ec73cd30.jpeg"
+  },
+  {
+    "revision": "5b44e09295f02f29c4d63468fde9ed2d",
+    "url": "/react-bootstrap-photographer-portfolio/static/media/img4.5b44e092.jpeg"
+  },
+  {
+    "revision": "adbef37a7c6372527f71110cf492441b",
+    "url": "/react-bootstrap-photographer-portfolio/static/media/img5.adbef37a.jpeg"
+  },
+  {
+    "revision": "dabaee8d6961551f101410928a20b91f",
+    "url": "/react-bootstrap-photographer-portfolio/static/media/img6.dabaee8d.jpeg"
+  },
+  {
+    "revision": "88d6ad295b31dfbc3194356128ad254a",
+    "url": "/react-bootstrap-photographer-portfolio/static/media/img7.88d6ad29.jpeg"
+  },
+  {
+    "revision": "a57672277711fd05f39674078cb5866f",
+    "url": "/react-bootstrap-photographer-portfolio/static/media/img8.a5767227.jpeg"
+  },
+  {
+    "revision": "e5f6e6bca6eae2eeb123272970e93fa5",
+    "url": "/react-bootstrap-photographer-portfolio/static/media/img9.e5f6e6bc.jpeg"
+  },
+  {
+    "revision": "e12a2b1da31a736e4c19038c9f41fabb",
+    "url": "/react-bootstrap-photographer-portfolio/static/media/nature.e12a2b1d.jpeg"
+  },
+  {
+    "revision": "b63f0e53325afe9a50bc94be5fc3e73a",
+    "url": "/react-bootstrap-photographer-portfolio/static/media/party.b63f0e53.jpeg"
+  },
+  {
+    "revision": "ac3bbee975b99a061fb4dfd60cc3c018",
+    "url": "/react-bootstrap-photographer-portfolio/static/media/pricing-card-bg.ac3bbee9.jpeg"
+  },
+  {
+    "revision": "1e14565c167383cf1c9b0016f3e7ba8c",
+    "url": "/react-bootstrap-photographer-portfolio/static/media/wedding.1e14565c.jpeg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5d7a5ffb91d9f5cb5918073d4b097202.js"
+  "/react-bootstrap-photographer-portfolio/precache-manifest.e99fe9467ec1dfa34419e06ebf605383.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-bootstrap-photographer-portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
